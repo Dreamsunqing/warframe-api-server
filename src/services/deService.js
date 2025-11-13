5f45eec2c16780d150d6a6e8d5248e5837d4e2a0
dream <dream_demo@outlook.com>
2025-11-13 15:37:42 +0800

    国际服：添加钢铁之路常驻奖励

--- src/services/deService.js
+++ src/services/deService.js
@@ -342,6 +342,103 @@ async function stellPathrewardProcess() {
         name: currentInfo.name,
         cost: currentInfo.cost,
       },
+      staticReward: [
+        {
+          name: "10,000 赤毒",
+          cost: 15,
+          count: 25,
+        },
+        {
+          name: "遗物组合包（3个）",
+          cost: 15,
+          count: 25,
+        },
+        {
+          name: "裂罅破解器",
+          cost: 20,
+          count: 1,
+        },
+        {
+          name: "军神护肩蓝图",
+          cost: 15,
+        },
+        {
+          name: "军神胸甲蓝图",
+          cost: 25,
+        },
+        {
+          name: "军神胫甲蓝图",
+          cost: 25,
+        },
+        {
+          name: "军神头甲蓝图",
+          cost: 20,
+        },
+        {
+          name: "架势 FORMA 蓝图",
+          cost: 10,
+        },
+        {
+          name: "三轨幻纹",
+          cost: 3,
+        },
+        {
+          name: "颅骨幻纹",
+          cost: 85,
+        },
+        {
+          name: "主要武器赋能槽连接器",
+          cost: 15,
+        },
+        {
+          name: "次要武器赋能槽连接器",
+          cost: 15,
+        },
+        {
+          name: "制衡",
+          cost: 85,
+        },
+        {
+          name: "Teshin摇头娃娃",
+          cost: 35,
+        },
+        {
+          name: "Gauss战斗姿态浮印",
+          cost: 15,
+        },
+        {
+          name: "Grendel战斗姿态浮印",
+          cost: 15,
+        },
+        {
+          name: "Protea战斗姿态浮印",
+          cost: 15,
+        },
+        {
+          name: "Xaku战斗姿态浮印",
+          cost: 15,
+        },
+        {
+          name: "Orokin 茶具",
+          cost: 15,
+        },
+        {
+          name: "The Teacher Earth Scene",
+          cost: 50,
+        },
+        {
+          name: "The Teacher Dojo Scene",
+          cost: 50,
+        },
+        {
+          name: "The Teacher Reactor Scene",
+          cost: 50,
+        },
+        {
+          name: "The Teacher Ship Scene",
+          cost: 50,
+        },
+      ],
     };
   })();
   return steelPath;
